5d0229d6e293df273a412646a84bc49481a92a64
JuanCruzV4 <83989306+JuanCruzV4@users.noreply.github.com>
2026-03-19 10:53:34 -0300

    Clean up print statements and improve email logic
    Removed print statements for script start, today's date, and birthday found. Updated email sending logic.

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import random
 import smtplib
 import os
 
-print("Script started")
 # import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
@@ -20,13 +19,11 @@ MY_PASSWORD = os.environ.get("MY_PASSWORD")
 today = datetime.now(timezone.utc)
 today_tuple = (today.month, today.day)
 
-print("Today's date:", today_tuple)
 
 data = pandas.read_csv("birthdays.csv")
-print("Birthdays loaded:", len(data))
+
 birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
 if today_tuple in birthdays_dict:
-    print("Birthday found!")
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
     with open(file_path) as letter_file:
@@ -41,7 +38,6 @@ try:
             to_addrs=birthday_person["email"],
             msg=f"Subject:Happy Birthday!\n\n{contents}"
         )
-        print("Email sent!")
 except Exception as e:
     print("Error sending email:", e)
     
